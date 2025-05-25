5d3ef5a6320588750e1d4f658c5f68dbb4b4f4ae
yasibena <118657398+yasibena@users.noreply.github.com>
2025-05-25 15:06:42 +0330

    [run prettier]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import styles from "./page.module.css";
 import IconComponent from "./components/icon/icon.component";
 
+import styles from "./page.module.css";
+
 export default function Page() {
-  return <div className={styles.page}>
-    <IconComponent color="red" name="ai"/>
-    Hello, friend!</div>;
+  return (
+    <div className={styles.page}>
+      <IconComponent color="red" name="ai" />
+      Hello, friend!
+    </div>
+  );
 }
