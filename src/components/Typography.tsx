aa199f79a56245438b72e4180afbb6e3ec15bb45
carozamani <carozamani@proton.me>
2025-05-25 21:10:21 +0330

    add props type in component

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,14 +1,15 @@
-import styles from "../styles/page.module.css";
+import styles from "../styles/typography.module.css";
 
 type Variant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'button' | 'overline' | 'body1' | 'body2'|'caption' ;
 
 type PropsTypography = {
-variant:Variant;
-};
+    variant: Variant;
+    children: React.ReactNode;
+    className: string;
+  };
 
-const Typography: React.FC<PropsTypography> = () => {
-  return (
-  );
+const Typography: React.FC<PropsTypography> = ({variant,children,className}) => {
+    return (zzz)
 };
 
 export default Typography;
