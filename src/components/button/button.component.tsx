4499f3d29ecf4b9a6976460da5929106618e567e
Rgh <gholamironak88@yahoo.com>
2025-05-25 12:12:27 +0330

    [CREATE] button component folder

---
+++ src/components/button/button.component.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+import styles from "./button.module.css";
+
+export default function ButtonComponent(): ReactElement {
+  return <div className={styles.button}></div>;
+}
