c8b07d19ce83814818b890c84e78050f5323cb32
Rgh <gholamironak88@yahoo.com>
2025-05-26 13:51:41 +0330

    [implement] html & css of button component

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,7 +1,47 @@
-import { ReactElement } from "react";
+import clsx from "clsx";
+
+import { ComponentProps, ReactElement } from "react";
 
 import styles from "./button.module.css";
 
-export default function ButtonComponent(): ReactElement {
-  return <div className={styles.button}></div>;
+type ButtonProps = {
+  variant?: "contained" | "outlined" | "text";
+  disabled?: boolean;
+  href?: string;
+  onClick?: () => void;
+  color?: "primary" | "inherit" | "info" | "success" | "warning" | "error";
+  size?: "small" | "medium" | "large";
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+};
+
+type Props = ComponentProps<"button"> & ButtonProps;
+
+export default function ButtonComponent({
+  variant = "contained",
+  disabled = false,
+  color = "primary",
+  size = "medium",
+  onClick,
+  startIcon,
+  endIcon,
+  children,
+  ...rest
+}: Props): ReactElement {
+  return (
+    <button
+      className={clsx(
+        styles.button,
+        styles[variant],
+        styles[size],
+        disabled ? styles.disabled : styles[color],
+      )}
+      onClick={disabled ? undefined : onClick}
+      {...rest}
+    >
+      {/*<IconComponent name={startIcon} />*/}
+      {children}
+      {/*<IconComponent name={endIcon} />*/}
+    </button>
+  );
 }
