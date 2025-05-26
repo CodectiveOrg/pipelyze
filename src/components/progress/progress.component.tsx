dcb4deddef57c1fa16fd49b51d265f66f94def5d
SinaMAmini <smobasheramini@gmail.com>
2025-05-26 15:06:34 +0330

    [CREATE] basic circular indeterminate progress

---
+++ src/components/progress/progress.component.tsx
@@ -0,0 +1,21 @@
+import { ReactElement } from "react";
+
+import styles from "./progress.module.css";
+
+export default function ProgressComponent(): ReactElement {
+  return (
+    <div className={styles.progress}>
+      <span role="progressbar">
+        <svg viewBox="22 22 44 44">
+          <circle
+            cx="44"
+            cy="44"
+            r="20.2"
+            fill="none"
+            stroke-width="3.6"
+          ></circle>
+        </svg>
+      </span>
+    </div>
+  );
+}
