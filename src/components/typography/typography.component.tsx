3ecf9b30fdadb368557b05a68589fa7349969e34
carozamani <carozamani@proton.me>
2025-05-27 17:04:48 +0330

    feat: add color prop to typographyComponent

--- src/components/typography/typography.component.tsx
+++ src/components/typography/typography.component.tsx
@@ -3,17 +3,20 @@ import clsx from "clsx";
 import { PropsWithChildren, ReactElement } from "react";
 
 import styles from './typography.module.css'
+import { TypographyType, Variant } from "@/types/typography.type";
 
 
 type Props = PropsWithChildren<{
   variant: Variant;
   className?: string;
+  color: TypographyType;
 }>;
 
 export default function TypographyComponent({
   variant,
   children,
   className,
+  color
 }: Props): ReactElement {
-  return <div className={clsx(className, variant)}>{children}</div>;
+  return <div className={clsx(className, variant, styles[color])}>{children}</div>;
 }
