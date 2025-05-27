01d813e893ba4bf3242fd1f0c29acf2b1fc2cb2e
yasibena <118657398+yasibena@users.noreply.github.com>
2025-05-27 13:01:08 +0330

    [run prettier]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import styles from "./page.module.css";
 export default function Page() {
   return (
     <div className={styles.page}>
-      <IconComponent name="ai-line"color="primary" />
+      <IconComponent name="ai-line" color="primary" />
       Hello, friend!
     </div>
   );
--- src/components/icon/icon.component.tsx
+++ src/components/icon/icon.component.tsx
@@ -1,11 +1,13 @@
 import clsx from "clsx";
 
 import { icons } from "@iconify-json/mingcute";
+
 import { Icon } from "@iconify/react";
 import { IconProps } from "@iconify/react";
 import { getIconData } from "@iconify/utils";
 
 import { ColorType } from "@/types/color.type";
+
 import styles from "./icon.module.css";
 
 type Props = Omit<IconProps, "icon" | "ssr" | "color"> & {
@@ -19,9 +21,8 @@ export default function IconComponent({
   inline = true,
   ...otherProps
 }: Props) {
-
   const iconData = getIconData(icons, name);
-  
+
   if (!iconData) {
     return (
       <svg height="1em" width="em" aria-hidden="true" viewBox="0 0 16 16"></svg>
