f723487909601c2972e28a9d5f3afacb8b7bfd14
yasibena <118657398+yasibena@users.noreply.github.com>
2025-05-27 14:20:50 +0330

    [resolve issue]

--- src/components/icon/icon.component.tsx
+++ src/components/icon/icon.component.tsx
@@ -28,7 +28,7 @@ export default function IconComponent({
     console.error(`Icon "${name}" is missing.`);
 
     return (
-      <svg height="1em" width="em" aria-hidden="true" viewBox="0 0 24 24"></svg>
+      <svg width="1em" height="1em" aria-hidden="true" viewBox="0 0 24 24"></svg>
     );
   }
 
