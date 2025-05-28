f8b025ed127526cc2d29b4bca52b3635635eb9a5
BijanProgrammer <bijaneisapour@gmail.com>
2025-05-28 08:46:40 +0330

    [MOVE] types to internal file

--- src/components/typography/typography.component.tsx
+++ src/components/typography/typography.component.tsx
@@ -2,14 +2,35 @@ import clsx from "clsx";
 
 import { PropsWithChildren, ReactElement } from "react";
 
-import { TypographyType, Variant } from "@/types/typography.type";
+import { ColorType } from "@/types/color.type";
 
 import styles from "./typography.module.css";
 
+type VariantType =
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+  | "subtitle1"
+  | "subtitle2"
+  | "body1"
+  | "body2"
+  | "caption"
+  | "overline"
+  | "button";
+
+export type TypographyColorType =
+  | ColorType
+  | "text-primary"
+  | "text-secondary"
+  | "text-disabled";
+
 type Props = PropsWithChildren<{
-  variant: Variant;
+  variant: VariantType;
   className?: string;
-  color: TypographyType;
+  color: TypographyColorType;
 }>;
 
 export default function TypographyComponent({
