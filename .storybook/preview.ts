941bbfe677fdebaf3def735e5155f26b887c8363
BijanProgrammer <bijaneisapour@gmail.com>
2025-05-28 13:04:13 +0330

    [ADD] typography docs

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,18 @@
+import { Inter } from "next/font/google";
+
 import type { Preview } from "@storybook/react";
 
 import "../src/app/globals.css";
 import "../src/styles/colors.css";
+import "../src/styles/shadows.css";
+import "../src/styles/typography.css";
+import "./preview.css";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
 
 const preview: Preview = {
   parameters: {
