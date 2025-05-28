72a271c56a115b8b883d3fbfffb265451b21edee
yasibena <118657398+yasibena@users.noreply.github.com>
2025-05-28 10:24:41 +0330

    [add landing page]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import TypographyComponent from "@/components/typography/typography.component";
+
 import styles from "./page.module.css";
 
 export default function Page() {
-  return <div className={styles.page}>Hello, friend!</div>;
+  return (
+    <div className={styles.page}>
+      <TypographyComponent variant="h2" color="text-disabled">
+        <span>Lorem ipsum dolor sit</span>
+      </TypographyComponent>
+
+      <TypographyComponent variant="h2">
+        Neque doloribus
+        <span className={styles.title}> pipelyze</span>
+      </TypographyComponent>
+
+      <TypographyComponent variant="subtitle1">
+        The starting point for your next project is based on MUI. Easy
+        <br />
+        customization helps you build apps faster and better.
+      </TypographyComponent>
+    </div>
+  );
 }
