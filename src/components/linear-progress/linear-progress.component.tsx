a09799877edce91da0e44c2b3bf2bbcd5f264f52
SinaMAmini <smobasheramini@gmail.com>
2025-05-31 10:42:06 +0330

    [FIX] determinate linear progress styles

--- src/components/linear-progress/linear-progress.component.tsx
+++ src/components/linear-progress/linear-progress.component.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import clsx from "clsx";
 
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 
 import { ColorType } from "@/types/color.type";
 
@@ -17,16 +19,21 @@ export default function LinearProgressComponent({
   determinate,
   value = 0,
 }: LinearProgressProps): ReactElement {
+  const initiatedValue: () => void = () => {
+    document.documentElement.style.setProperty(
+      "--progress-value",
+      value - 100 + "%",
+    );
+  };
+
+  useEffect(() => {
+    initiatedValue();
+  }, []);
   return (
     <div className={clsx(styles["linear-progress"], styles[color])}>
       {determinate ? (
         <span className={styles["determinate-progress-line"]}>
-          <span
-            className={styles["filled"]}
-            style={{
-              transform: `translateX(${value - 100}%)`,
-            }}
-          ></span>
+          <span className={styles["filled"]}></span>
         </span>
       ) : (
         <span className={styles["indeterminate-progress-line"]}></span>
