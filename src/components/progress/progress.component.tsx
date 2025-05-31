6d1d222327b36dfab23ef0ca51a0f3b5e00e46d8
SinaMAmini <smobasheramini@gmail.com>
2025-05-31 14:57:32 +0330

    [FIX] determinate circular progress show up

--- src/components/progress/progress.component.tsx
+++ src/components/progress/progress.component.tsx
@@ -1,31 +1,65 @@
-import { ReactElement } from "react";
+"use client";
+
+import clsx from "clsx";
+
+import { ReactElement, useEffect } from "react";
+
+import { ColorType } from "@/types/color.type";
 
 import styles from "./progress.module.css";
 
-export default function ProgressComponent(prop: {
-  color: string;
+type ProgressProps = {
+  color?: ColorType;
+  determinate: boolean;
   size: number;
-}): ReactElement {
+  value?: number;
+};
+
+export default function ProgressComponent({
+  color = "primary",
+  size,
+  determinate,
+  value = 0,
+}: ProgressProps): ReactElement {
+  const initiatedValue: () => void = () => {
+    document.documentElement.style.setProperty(
+      "--filled-circle",
+      100 - value + "px",
+    );
+    document.documentElement.style.setProperty("--size", size + "rem");
+  };
+
+  useEffect(() => {
+    initiatedValue();
+  }, []);
+
   return (
-    <div className={styles.progress}>
-      <span
-        style={{
-          color: prop.color,
-          inlineSize: `${prop.size}rem`,
-          blockSize: `${prop.size}rem`,
-        }}
-        role="progressbar"
-      >
-        <svg viewBox="22 22 44 44">
-          <circle
-            cx="44"
-            cy="44"
-            r="20.2"
-            fill="none"
-            stroke-width="3.6"
-          ></circle>
-        </svg>
-      </span>
+    <div className={clsx(styles.progress, styles[color])}>
+      {determinate ? (
+        <span className={styles["determinant-progress"]}>
+          <svg viewBox="22 22 44 44">
+            <circle
+              cx="44"
+              cy="44"
+              r={100 / 2 / Math.PI}
+              fill="none"
+              stroke-width="3.6"
+            ></circle>
+          </svg>
+        </span>
+      ) : (
+        <span className={styles["indeterminate-progress"]}>
+          <svg viewBox="22 22 44 44">
+            <circle
+              cx="44"
+              cy="44"
+              r={100 / 2 / Math.PI}
+              fill="none"
+              stroke-width="3.6"
+            ></circle>
+          </svg>
+        </span>
+      )}
     </div>
   );
 }
