9e33c727dccad88cdae698c534e5559720afd8b3
Rgh <gholamironak88@yahoo.com>
2025-05-31 15:06:04 +0330

    [UPDATE] button storybook

--- src/components/button/button.stories.ts
+++ src/components/button/button.stories.ts
@@ -36,17 +36,20 @@ type Story = StoryObj<typeof meta>;
 export const Contained: Story = {
   args: {
     variant: "contained",
+    children: "click me",
   },
 };
 
 export const Outlined: Story = {
   args: {
     variant: "outlined",
+    children: "click me",
   },
 };
 
 export const Text: Story = {
   args: {
     variant: "text",
+    children: "click me",
   },
 };
