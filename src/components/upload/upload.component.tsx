25b32ad3a47a54198616729597efa4af0e8e9309
Rgh <gholamironak88@yahoo.com>
2025-06-01 13:36:06 +0330

    [ADD] upload component

---
+++ src/components/upload/upload.component.tsx
@@ -0,0 +1,24 @@
+import { ReactElement } from "react";
+
+import UploadSvg from "@/assets/Upload.svg";
+
+import styles from "./upload.module.css";
+
+export default function UploadComponent(): ReactElement {
+  return (
+    <div className={styles.upload}>
+      <label>
+        <input tabIndex={-1} type="file" />
+        <div className={styles.text}>
+          <UploadSvg className={styles.img} />
+          <div className={styles.placeholder}>
+            <div className={styles.title}>Drop or select file</div>
+            <div className={styles.description}>
+              Drop files here or click to browse through your machine.
+            </div>
+          </div>
+        </div>
+      </label>
+    </div>
+  );
+}
