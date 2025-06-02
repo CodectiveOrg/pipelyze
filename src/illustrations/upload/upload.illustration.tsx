5312a3a010aff9e5aa9df64773b9b28a86fdaac5
Rgh <gholamironak88@yahoo.com>
2025-06-02 10:26:27 +0330

    [UPDATE] remove redundant tag & css

--- src/illustrations/upload/upload.illustration.tsx
+++ src/illustrations/upload/upload.illustration.tsx
@@ -2,23 +2,25 @@ import { ReactElement } from "react";
 
 import UploadSvg from "@/assets/illustrations/Upload.svg";
 
+import TypographyComponent from "@/components/typography/typography.component";
+
 import styles from "./upload.illustration.module.css";
 
 export default function UploadIllustration(): ReactElement {
   return (
-    <div className={styles.upload}>
-      <label>
-        <input tabIndex={-1} type="file" />
-        <div className={styles.text}>
-          <UploadSvg className={styles.img} />
-          <div className={styles.placeholder}>
-            <div className={styles.title}>Drop or select file</div>
-            <div className={styles.description}>
-              Drop files here or click to browse through your machine.
-            </div>
-          </div>
-        </div>
-      </label>
-    </div>
+    <label className={styles.upload}>
+      <input tabIndex={-1} type="file" />
+      <UploadSvg className={styles.img} />
+      <TypographyComponent variant="h6">
+        Drop or select file
+      </TypographyComponent>
+      <TypographyComponent
+        variant="body2"
+        color="text-secondary"
+        className={styles.link}
+      >
+        Drop files here or click to<span>browse</span>through your machine.
+      </TypographyComponent>
+    </label>
   );
 }
