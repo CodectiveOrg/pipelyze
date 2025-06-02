6e0e5a4e48bd1233ec16dd5e2476e304a253b22a
BijanProgrammer <bijaneisapour@gmail.com>
2025-06-02 14:04:24 +0330

    [REVERT] unnecessary changes

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ["pub-c5e31b5cdafb419fb247a8ac2e78df7a.r2.dev"],
-  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import AvatarComponent from "@/components/avatar/avatar.component";
-
 import styles from "./page.module.css";
 
 export default function Page() {
-  return (
-    <div className={styles.page}>
-      Hello, friend!
-      <div>
-        <AvatarComponent name="A" />
-        <AvatarComponent name="Bijan Eisapour" />
-        <AvatarComponent name="C" variant="rounded" />
-        <AvatarComponent name="D" variant="square" />
-        <AvatarComponent name="E" size={2} />
-        <AvatarComponent name="F" size={1.5} />
-        <AvatarComponent name="G" size={1} />
-      </div>
-    </div>
-  );
+  return <div className={styles.page}>Hello, friend!</div>;
 }
