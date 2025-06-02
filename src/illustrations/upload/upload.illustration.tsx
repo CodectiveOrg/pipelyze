83b20a4339b6f181c1edbe8ac0c0bfcd6e208b87
Rgh <gholamironak88@yahoo.com>
2025-06-02 11:14:56 +0330

    [ADD] svg.illustration.module.css

--- src/illustrations/upload/upload.illustration.tsx
+++ src/illustrations/upload/upload.illustration.tsx
@@ -4,13 +4,14 @@ import UploadSvg from "@/assets/illustrations/Upload.svg";
 
 import TypographyComponent from "@/components/typography/typography.component";
 
+import svgStyles from "./svg.illustration.module.css";
 import styles from "./upload.illustration.module.css";
 
 export default function UploadIllustration(): ReactElement {
   return (
     <label className={styles.upload}>
       <input tabIndex={-1} type="file" />
-      <UploadSvg className={styles.img} />
+      <UploadSvg className={svgStyles.svg} />
       <TypographyComponent variant="h6">
         Drop or select file
       </TypographyComponent>
