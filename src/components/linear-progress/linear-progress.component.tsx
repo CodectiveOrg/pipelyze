509648959674aacb0947ad2395bc2b7ad2a987dc
Sina Amini <91621769+SinaMAmini@users.noreply.github.com>
2025-06-03 09:29:25 +0330

    BI-6 Progress Component (#10)
    * [CREATE] basic circular indeterminate progress
    
    * [CREATE] basic linear indeterminate progress
    
    * [ADD] size and color to prop
    
    * [FIX] linear progress and css properties
    
    * [FIX] linear progress and css properties
    
    * [ADD] determinate linear progress
    
    * [FIX] linear progress show up
    
    * [FIX] determinate linear progress styles
    
    * [FIX] determinate circular progress show up
    
    * [FIX] determinate circular progress show up
    
    * [FIX] indeterminate circular progress show up
    
    * [FIX] initial value of css properties
    
    * [FIX] prettier
    
    * [ADD] .gitattributes
    
    * [TEMP] fix pr lint error
    
    * [TEMP] fix pr lint error
    
    * [CHANGE] folder name
    
    * [UPDATE] prettier scripts
    
    * [PRETTIER]
    
    * [FIX] stylelint
    
    * [REMOVE] duplicate elements and styles
    
    * [FIX] variant type for progresses
    
    * [FIX] circular progress pr issues
    
    * [FIX] linear progress pr issues
    
    * [ADD] storybooks
    
    * [FIX] storybooks
    
    * [FIX] storybooks color issue
    
    * [UPDATE] bad variables name
    
    * [FIX] css variables name
    
    ---------
    
    Co-authored-by: BijanProgrammer <bijaneisapour@gmail.com>

---
+++ src/components/linear-progress/linear-progress.component.tsx
@@ -0,0 +1,34 @@
+import { CSSProperties, ReactElement } from "react";
+
+import clsx from "clsx";
+
+import { ColorType } from "@/types/color.type";
+
+import styles from "./linear-progress.module.css";
+
+type Props = {
+  color?: ColorType;
+  variant: "determinate" | "indeterminate";
+  value?: number;
+};
+
+export default function LinearProgressComponent({
+  color = "inherit",
+  variant,
+  value = 0,
+}: Props): ReactElement {
+  return (
+    <div
+      className={clsx(styles["linear-progress"], color, styles[variant])}
+      style={{ "--value": value - 100 + "%" } as CSSProperties}
+    >
+      {variant === "determinate" ? (
+        <span className={styles.line}>
+          <span className={styles.filled}></span>
+        </span>
+      ) : (
+        <span className={styles.line}></span>
+      )}
+    </div>
+  );
+}
