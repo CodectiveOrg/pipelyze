c09dff1cabc232fdfa853a8cda55720628124a7f
carozamani <carozamani@proton.me>
2025-06-03 13:07:33 +0330

    clened cod in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,5 @@
-"use client";
-
-import { FormEvent, useRef } from "react";
-
-import RadioGroupComponent from "@/components/radio-group/radio-group.component";
+import styles from "./page.module.css";
 
 export default function Page() {
-  const formRef = useRef<HTMLFormElement>(null);
-
-  const formSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData(formRef.current!);
-    console.log(formData.get("fruit"));
-  };
-
-  return (
-    <form ref={formRef} onSubmit={formSubmitHandler}>
-      <RadioGroupComponent
-        name="fruit"
-        items={[
-          { value: "apple", label: "Apple" },
-          { value: "orange", label: "Orange" },
-          { value: "banana", label: "Banana" },
-          { value: "disabled", label: "Disabled", disabled: true },
-        ]}
-      />
-      <RadioGroupComponent
-        name="size"
-        items={[
-          { value: "small", label: "Small", size: "small" },
-          { value: "normal", label: "Normal" },
-        ]}
-      />
-      <button>Submit</button>
-    </form>
-  );
+  return <div className={styles.page}>Hello, friend!</div>;
 }
