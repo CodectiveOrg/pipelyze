01b23edbe6fb782994ba8c0ed35cd787e928a8f0
carozamani <carozamani@proton.me>
2025-06-06 20:29:36 +0330

    style: fix and debug inline size for wrapper element

--- src/components/text-field-Groupe/text-field-groupe.stories.ts
+++ src/components/text-field-Groupe/text-field-groupe.stories.ts
@@ -1,17 +1,15 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import ComponentName from "./ComponentName.component";
+import TextFieldGroupeComponent from "./text-field-groupe.component";
 
-const meta: Meta<typeof ComponentName> = {
-  title: "Components/ComponentName",
-  component: ComponentName,
-  tags: ["autodocs"],
-  argTypes: {
-    disabled: { control: "boolean" },
-    error: { control: "boolean" },
-    label: { control: "text" },
-    value: { control: "text" },
+const meta: Meta<typeof TextFieldGroupeComponent> = {
+  title: "Atoms/TextFieldGroupe",
+  component: TextFieldGroupeComponent,
+  parameters: {
+    layout: "centered",
   },
+  tags: ["autodocs"],
+  argTypes: {},
 };
 
 export default meta;
@@ -20,9 +18,17 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     label: "Username",
-    value: "",
-    disabled: false,
-    error: false,
+
+    items: [
+      { label: "Default", color: "inherit", size: "normal" },
+      { label: "Primary", color: "primary", size: "normal" },
+      { label: "Secondary", color: "secondary", size: "normal" },
+      { label: "Info", color: "info", size: "normal" },
+      { label: "Success", color: "success", size: "normal" },
+      { label: "Warning", color: "warning", size: "normal" },
+      { label: "Error", color: "error", size: "normal" },
+      { label: "Disabled", size: "normal", disabled: true },
+    ],
   },
 };
 
@@ -35,8 +41,17 @@ export const Disabled: Story = {
 
 export const WithError: Story = {
   args: {
-    ...Default.args,
-    error: true,
-    helperText: "This field is required",
+    name: "color",
+    row: false,
+    items: [
+      { label: "Default", color: "inherit", size: "normal" },
+      { label: "Primary", color: "primary", size: "normal" },
+      { label: "Secondary", color: "secondary", size: "normal" },
+      { label: "Info", color: "info", size: "normal" },
+      { label: "Success", color: "success", size: "normal" },
+      { label: "Warning", color: "warning", size: "normal" },
+      { label: "Error", color: "error", size: "normal" },
+      { label: "Disabled", size: "normal", disabled: true },
+    ],
   },
 };
