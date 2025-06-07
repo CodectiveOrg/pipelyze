f50782b82798605ae6097876214cabcf31884387
SinaMAmini <smobasheramini@gmail.com>
2025-06-07 12:15:53 +0330

    [REMAKE] nav styles

---
+++ src/components/navigation-bar/navigation-bar.component.tsx
@@ -0,0 +1,61 @@
+import { ReactElement } from "react";
+
+import IconButtonComponent from "@/components/icon-button/icon-button.component";
+import IconComponent from "@/components/icon/icon.component";
+
+import styles from "./navigation-bar.module.css";
+
+type NavItem = {
+  title: string;
+  icon: ReactElement<typeof IconComponent>;
+  subtitle?: string;
+};
+
+type Props = {
+  items: NavItem[];
+};
+
+export default function NavigationBarComponent({ items }: Props): ReactElement {
+  return (
+    <div className={styles["navigation-bar"]}>
+      <IconButtonComponent
+        className={styles.hamburger}
+        name={"menu-line"}
+      ></IconButtonComponent>
+      <nav className={styles.nav}>
+        <ul>
+          {items.map((item) => {
+            return (
+              <li key={item.title}>
+                <a href="https://google.com">
+                  {item.icon}
+                  <span>
+                    <span>{item.title}</span>
+                    <span>{item?.subtitle}</span>
+                  </span>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      <nav className={styles["vertical-nav"]}>
+        <ul>
+          {items.map((item) => {
+            return (
+              <li key={item.title}>
+                <a href="https://google.com">
+                  {item.icon}
+                  <span>
+                    <span>{item.title}</span>
+                    <span>{item?.subtitle}</span>
+                  </span>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
+}
