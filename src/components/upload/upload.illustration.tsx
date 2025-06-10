7c56fd859b35a57f74f0e7ead08e4d35c952857d
Rgh <gholamironak88@yahoo.com>
2025-06-10 09:15:35 +0330

    [UPDATE] add props & remove func

--- src/components/upload/upload.illustration.tsx
+++ src/components/upload/upload.illustration.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactElement } from "react";
+import { ReactElement, useCallback, useState } from "react";
 
 import UploadSvg from "@/illustrations/upload/upload.illustration";
 import { useDropzone } from "react-dropzone";
@@ -12,35 +12,79 @@ import TypographyComponent from "@/components/typography/typography.component";
 
 import styles from "./upload.module.css";
 
-export default function UploadIllustration(): ReactElement {
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+type Props = {
+  maxSize?: number; //Maximum file size (in bytes)
+  minSize?: number; //Minimum file size (in bytes)
+  accept?: {
+    "image/*": [".jpeg", ".jpg", ".png", ".gif", ".webp"];
+    "text/*": [".pdf", ".doc", ".docx", ".txt"];
+  };
+};
+
+export default function UploadIllustration({
+  maxSize,
+  minSize,
+  accept,
+}: Props): ReactElement {
+  const [file, setFile] = useState<File | null>(null);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setFile(acceptedFiles[0]);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
     maxFiles: 1,
+    maxSize: maxSize,
+    minSize: minSize,
+    accept: accept,
+    onDrop,
   });
 
-  const files = acceptedFiles.map((file) => (
-    <li key={file.path}>
-      <span>
-        <IconComponent
-          name="document-2-fill"
-          fontSize="2rem"
-          color={"primary"}
+  function removeFile() {
+    setFile(null);
+  }
+
+  const preview = file ? (
+    <div className={styles["selected-file-container"]}>
+      <div className={styles["selected-file"]}>
+        <span>
+          <IconComponent name="document-2-fill" fontSize="2rem" />
+        </span>
+        <div className={styles["file-details"]}>
+          <TypographyComponent variant="subtitle2">
+            {file.name}
+          </TypographyComponent>
+          <TypographyComponent variant="caption">
+            {file.size} kb
+          </TypographyComponent>
+        </div>
+        <IconButtonComponent
+          name="close-fill"
+          color="inherit"
+          onClick={removeFile}
+          className={styles.btn}
         />
-      </span>
-      <div className={styles["file-details"]}>
-        <TypographyComponent variant="subtitle2">
-          {file.name}
-        </TypographyComponent>
-        <TypographyComponent variant="caption">
-          {file.size} kb
-        </TypographyComponent>
       </div>
-      <IconButtonComponent
-        name="close-fill"
-        color="inherit"
-        className={styles.btn}
-      />
-    </li>
-  ));
+      <div className={styles.action}>
+        <ButtonComponent
+          fontSize={"small"}
+          color={"inherit"}
+          variant={"outlined"}
+          onClick={removeFile}
+        >
+          Remove all
+        </ButtonComponent>
+        <ButtonComponent
+          fontSize={"small"}
+          color={"inherit"}
+          variant={"contained"}
+          startIcon="upload-3-fill"
+        >
+          Upload
+        </ButtonComponent>
+      </div>
+    </div>
+  ) : null;
 
   return (
     <>
@@ -58,28 +102,7 @@ export default function UploadIllustration(): ReactElement {
           Drop files here or click to <span>browse</span> through your machine.
         </TypographyComponent>
       </div>
-      {files.length > 0 && (
-        <div className={styles["selected-file"]}>
-          <ul>{files}</ul>
-          <div className={styles.action}>
-            <ButtonComponent
-              fontSize={"small"}
-              color={"inherit"}
-              variant={"outlined"}
-            >
-              Remove all
-            </ButtonComponent>
-            <ButtonComponent
-              fontSize={"small"}
-              color={"inherit"}
-              variant={"contained"}
-              startIcon="upload-3-fill"
-            >
-              Upload
-            </ButtonComponent>
-          </div>
-        </div>
-      )}
+      {preview}
     </>
   );
 }
