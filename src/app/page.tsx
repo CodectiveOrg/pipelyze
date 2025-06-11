f1a25f5a1e14bd51a75b2c3b22ea1c6ba7a6a723
yasibena <118657398+yasibena@users.noreply.github.com>
2025-06-11 08:44:04 +0330

    [add checkbox]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import CheckBoxComponent from "@/components/checkbox/checkbox.component";
 import styles from "./page.module.css";
 
 export default function Page() {
-  return <div className={styles.page}>Hello, friend!</div>;
+  return <div className={styles.page}>
+    <CheckBoxComponent/>
+    Hello, friend!</div>;
 }
--- src/components/checkbox/checkbox.component.tsx
+++ src/components/checkbox/checkbox.component.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { ComponentProps, ReactElement, useState } from "react";
+import { ComponentProps, ReactElement } from "react";
 
 import clsx from "clsx";
 
-import RippleComponent from "@/components/checkbox/components/ripple/ripple.component";
 import TypographyComponent from "@/components/typography/typography.component";
 
 import { ColorType } from "@/types/color.type";
@@ -24,33 +23,16 @@ export default function CheckBoxComponent({
   className,
   ...otherProps
 }: Props): ReactElement {
-  const [ripples, setRipples] = useState<number[]>([]);
-
   const isDisabled = otherProps.disabled;
-
-  const activeRipple = () => {
-    const id = Date.now();
-
-    setRipples((prev) => [...prev, id]);
-
-    setTimeout(() => {
-      setRipples((prev) => prev.filter((prevRipple) => prevRipple !== id));
-    }, 550);
-  };
 
   return (
     <label className={clsx(styles.checkbox, styles[size], color, className)}>
       <div className={styles.control}>
-        {ripples.map((ripple) => (
-          <RippleComponent key={ripple} />
-        ))}
         <input
           type="checkbox"
-          onClick={activeRipple}
           className={clsx(isDisabled && styles.disabled)}
           {...otherProps}
         />
-        <RippleComponent className={styles["active-ripple"]} />
       </div>
       <TypographyComponent variant="body2" color="inherit">
         {label}
