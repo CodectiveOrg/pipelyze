b9a7a481b8715905f6598413d9971ea98711453a
BijanProgrammer <bijaneisapour@gmail.com>
2025-06-15 12:06:58 +0330

    [FIX] small issues

--- src/components/checkbox/checkbox.component.tsx
+++ src/components/checkbox/checkbox.component.tsx
@@ -35,9 +35,8 @@ export default function CheckboxComponent({
         />
         <IconComponent
           name="check-fill"
-          color="primary"
-          className={styles["check-icon"]}
-          style={{ color: "#ffff" }}
+          color="inherit"
+          className={styles.icon}
         />
       </span>
       <TypographyComponent variant="body2">{label}</TypographyComponent>
