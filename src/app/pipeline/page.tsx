e62a5a45fee1d79d264e3e261c26b2695581a496
SinaMAmini <smobasheramini@gmail.com>
2025-07-30 13:23:07 +0330

    [ADD] resizable section

--- src/app/pipeline/page.tsx
+++ src/app/pipeline/page.tsx
@@ -4,6 +4,8 @@ import { ReactNode, useState } from "react";
 
 import clsx from "clsx";
 
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+
 import styles from "./page.module.css";
 
 export default function Page(): ReactNode {
@@ -14,15 +16,31 @@ export default function Page(): ReactNode {
   }
 
   return (
-    <div className={styles["pipeline-page"]}>
+    <div
+      className={clsx(
+        styles["pipeline-page"],
+        !showSidebar && styles["closed-sidebar"],
+      )}
+    >
       <nav>Navbar</nav>
-      <div className={clsx(styles.sidebar, !showSidebar && styles.closed)}>
-        Sidebar
+      <div className={clsx(styles.sidebar)}>
         <button onClick={() => toggleSidebar()}>
           {showSidebar ? "<" : ">"}
         </button>
       </div>
-      <main>Main Content</main>
+      <main>
+        <PanelGroup direction={"vertical"}>
+          <Panel>
+            <div className={styles["pipeline-section"]}>
+              It is pipeline section
+            </div>
+          </Panel>
+          <PanelResizeHandle className={styles["line-in-between"]} />
+          <Panel>
+            <div className={styles["preview"]}>It is preview</div>
+          </Panel>
+        </PanelGroup>
+      </main>
     </div>
   );
 }
