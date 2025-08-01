f66a19ae08fb52e47d7b3e76ac75fbf594b932df
ZahraZare <zahrazare3099@gmail.com>
2025-08-01 18:57:56 +0330

    [ADD] add format file size handler

---
+++ src/components/file-manager/_utils/formatFileSize.tsx
@@ -0,0 +1,14 @@
+export default function formatFileSize(bytes: number): string {
+  if (bytes < 0) return "Invalid size";
+
+  const units = ["B", "KB", "MB", "GB", "TB", "PB", "EB"];
+  let size = bytes;
+  let unitIndex = 0;
+
+  while (size >= 1000 && unitIndex < units.length - 1) {
+    size = size / 1000;
+    unitIndex++;
+  }
+
+  return `${size.toFixed(1)} ${units[unitIndex]}`;
+}
