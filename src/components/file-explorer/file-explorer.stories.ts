fd486514de86d4587b8002b64dd73298b26258d7
mazaherireza <rezamazaheri.email@gmail.com>
2025-08-04 02:53:04 +0330

    [UPDATE] storybook

--- src/components/file-explorer/file-explorer.stories.ts
+++ src/components/file-explorer/file-explorer.stories.ts
@@ -1,20 +1,20 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import FileManagerComponent from "./file-explorer.component";
+import FileExplorerComponent from "./file-explorer.component";
 
 const meta = {
-  title: "Components/FileManagerComponent",
-  component: FileManagerComponent,
+  title: "Components/FileExplorerComponent",
+  component: FileExplorerComponent,
   parameters: {
     layout: "fullscreen",
   },
   tags: ["autodocs"],
   args: {},
-} satisfies Meta<typeof FileManagerComponent>;
+} satisfies Meta<typeof FileExplorerComponent>;
 
 export default meta;
 
-type Story = StoryObj<typeof FileManagerComponent>;
+type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {},
