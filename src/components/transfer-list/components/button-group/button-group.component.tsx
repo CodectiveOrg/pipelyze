be5f323d85a54a8e3a341c8aef2574a3f59b3f58
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-04 11:48:26 +0330

    [ADD] list button component

--- src/components/transfer-list/components/button-group/button-group.component.tsx
+++ src/components/transfer-list/components/button-group/button-group.component.tsx
@@ -19,42 +19,55 @@ export default function ButtonGroupComponent({
 }: Props): ReactNode {
   return (
     <div className={styles["button-group"]}>
-      <ButtonComponent
-        variant="outlined"
-        color="inherit"
-        disabled={!items.some((item) => item.position === "left")}
+      <ListButton
+        items={items}
+        icon="arrows-right-line"
+        enabled={(item) => item.position === "left"}
         onClick={() => onMoveAllItems("right")}
-      >
-        <IconComponent name="arrows-right-line" />
-      </ButtonComponent>
-      <ButtonComponent
-        variant="outlined"
-        color="inherit"
-        disabled={
-          !items.some((item) => item.position === "left" && item.isChecked)
-        }
+      />
+      <ListButton
+        items={items}
+        icon="right-line"
+        enabled={(item) => item.position === "left" && item.isChecked}
         onClick={() => onMoveCheckedItems("right")}
-      >
-        <IconComponent name="right-line" />
-      </ButtonComponent>
-      <ButtonComponent
-        variant="outlined"
-        color="inherit"
-        disabled={
-          !items.some((item) => item.position === "right" && item.isChecked)
-        }
+      />
+      <ListButton
+        items={items}
+        icon="left-line"
+        enabled={(item) => item.position === "right" && item.isChecked}
         onClick={() => onMoveCheckedItems("left")}
-      >
-        <IconComponent name="left-line" />
-      </ButtonComponent>
-      <ButtonComponent
-        variant="outlined"
-        color="inherit"
-        disabled={!items.some((item) => item.position === "right")}
+      />
+      <ListButton
+        items={items}
+        icon="arrows-left-line"
+        enabled={(item) => item.position === "right"}
         onClick={() => onMoveAllItems("left")}
-      >
-        <IconComponent name="arrows-left-line" />
-      </ButtonComponent>
+      />
     </div>
+  );
+}
+
+type ListButtonProps = {
+  items: readonly Item[];
+  icon: string;
+  enabled: (item: Item) => boolean;
+  onClick: () => void;
+};
+
+function ListButton({
+  items,
+  icon,
+  enabled,
+  onClick,
+}: ListButtonProps): ReactNode {
+  return (
+    <ButtonComponent
+      variant="outlined"
+      color="inherit"
+      disabled={!items.some(enabled)}
+      onClick={onClick}
+    >
+      <IconComponent name={icon} />
+    </ButtonComponent>
   );
 }
