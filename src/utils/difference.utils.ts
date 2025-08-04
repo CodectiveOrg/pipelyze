11719671c6901c3b470eb943e5501ab6b8be10e9
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-04 11:48:46 +0330

    [REMOVE] redundant files

--- src/utils/difference.utils.ts
+++
@@ -1,6 +0,0 @@
-export function getDifference<T>(
-  totalItems: readonly T[],
-  otherItems: readonly T[],
-): T[] {
-  return totalItems.filter((value) => !otherItems.includes(value));
-}
--- src/utils/intersection.utils.ts
+++
@@ -1,6 +0,0 @@
-export function getIntersection<T>(
-  selectedItems: readonly T[],
-  totalItems: readonly T[],
-): T[] {
-  return selectedItems.filter((value) => totalItems.includes(value));
-}
