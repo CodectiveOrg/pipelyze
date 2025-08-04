dea2e7d61ea8d97bf3681ebd334a8aabba03196b
mazaherireza <rezamazaheri.email@gmail.com>
2025-08-04 03:53:21 +0330

    [UPDATE] storybook

--- src/components/file-explorer/file-explorer.stories.ts
+++ src/components/file-explorer/file-explorer.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import FileExplorerComponent from "./file-explorer.component";
 
 const meta = {
-  title: "Components/FileExplorerComponent",
+  title: "Components/FileExplorer",
   component: FileExplorerComponent,
   parameters: {
     layout: "fullscreen",
