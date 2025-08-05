1283e91809b8dc188e888e3a274e84df4b53385d
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-05 09:45:51 +0330

    [REFACTOR] row styles

--- src/components/file-explorer/components/file-explorer-row/file-explorer-row.component.tsx
+++ src/components/file-explorer/components/file-explorer-row/file-explorer-row.component.tsx
@@ -1,8 +1,11 @@
 import type { ReactNode } from "react";
 
+import clsx from "clsx";
+
 import FileExplorerThumbnailComponent from "@/components/file-explorer/components/file-explorer-thumbnail/file-explorer-thumbnail.component";
 import { FileExplorerItemType } from "@/components/file-explorer/type/file-explorer-item.type";
 import IconButtonComponent from "@/components/icon-button/icon-button.component";
+import TypographyComponent from "@/components/typography/typography.component";
 
 import {
   dateFormatter,
@@ -18,20 +21,20 @@ type Props = {
 
 export default function FileExplorerRowComponent({ item }: Props): ReactNode {
   return (
-    <div className={styles["file-explorer-row"]}>
-      <div>
+    <div className={clsx(styles["file-explorer-row"], "body2")}>
+      <div className={clsx(styles.cell, styles.title)}>
         <FileExplorerThumbnailComponent extension={item.extension} />
         {item.title}
       </div>
-      <div>{formatFileSize(item.size)}</div>
-      <div>{item.extension}</div>
-      <div>
+      <div className={styles.cell}>{formatFileSize(item.size)}</div>
+      <div className={styles.cell}>{item.extension}</div>
+      <div className={clsx(styles.cell, styles.datetime)}>
         <div>{dateFormatter.format(item.modifiedAt)}</div>
-        <div className={styles.time}>
+        <TypographyComponent variant="caption" color="text-secondary">
           {hourMinuteFormatter.format(item.modifiedAt)}
-        </div>
+        </TypographyComponent>
       </div>
-      <div>
+      <div className={clsx(styles.cell, styles.actions)}>
         <IconButtonComponent name="star-line" size="small" color="action" />
         <IconButtonComponent name="more-2-line" size="small" color="action" />
       </div>
