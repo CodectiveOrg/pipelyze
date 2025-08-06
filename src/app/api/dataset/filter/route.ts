5aaa7151d3ecffe3cef3880829613cca784d5903
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2025-08-06 12:08:55 +0330

    fix pr review feedback

--- src/app/api/dataset/filter/route.ts
+++ src/app/api/dataset/filter/route.ts
@@ -2,45 +2,38 @@ import { NextRequest, NextResponse } from "next/server";
 
 import prisma from "@/lib/prisma";
 
-type Records = { id: number; filename: string; title: string | null }[];
-type Body = { page: number; take: number };
+type Dataset = { id: number; filename: string; title: string | null };
+type Body = { page: number; size: number };
 type Res = Promise<
   NextResponse<
-    | {
-        data: {
-          data: Records;
-          first: number;
-          last: number;
-          currentPage: number;
-        };
-      }
+    | { result: { datasets: Dataset[]; pagesCount: number; page: number } }
     | { error: string }
   >
 >;
 
 export const POST = async (req: NextRequest): Res => {
-  const { page, take }: Body = await req.json();
+  const { page, size }: Body = await req.json();
 
   if (page < 1) {
     return NextResponse.json({ error: "Invalid page number" }, { status: 400 });
   }
 
-  const skip = (page - 1) * take;
-
   try {
-    const count = await prisma.dataset.count();
-    const records = await prisma.dataset.findMany({ take, skip });
+    const datasets = await prisma.dataset.findMany({
+      take: size,
+      skip: (page - 1) * size,
+    });
 
-    const pagesCount = Math.ceil(count / take);
+    const datasetsCount = await prisma.dataset.count();
+    const pagesCount = Math.ceil(datasetsCount / size);
 
-    const data = {
-      data: records,
-      first: pagesCount && 1,
-      last: pagesCount,
-      currentPage: page,
+    const result = {
+      datasets,
+      pagesCount: pagesCount,
+      page,
     };
 
-    return NextResponse.json({ data }, { status: 200 });
+    return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: "Something went wrong.", status: 500 });
