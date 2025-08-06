6ff716941e6e873fbfb73b4dd41239dd87c4a628
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-06 14:56:59 +0330

    [REFACTOR] pipeline page

--- src/app/pipeline/page.tsx
+++ src/app/pipeline/page.tsx
@@ -6,41 +6,39 @@ import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 import clsx from "clsx";
 
+import ButtonComponent from "@/components/button/button.component";
+
 import styles from "./page.module.css";
 
 export default function Page(): ReactNode {
-  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true);
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
 
   const toggleSidebar = (): void => {
-    setIsSidebarOpen((old) => !old);
+    setIsCollapsed((old) => !old);
   };
 
   return (
-    <div
-      className={clsx(
-        styles["pipeline-page"],
-        !isSidebarOpen && styles["closed-sidebar"],
-      )}
-    >
-      <nav>Navbar</nav>
-      <div className={clsx(styles.sidebar)}>
-        <button onClick={() => toggleSidebar()}>
-          {isSidebarOpen ? "<" : ">"}
-        </button>
-      </div>
-      <main>
-        <PanelGroup direction={"vertical"}>
-          <Panel>
-            <div className={styles["pipeline-section"]}>
-              It is pipeline section
-            </div>
-          </Panel>
-          <PanelResizeHandle className={styles["line-in-between"]} />
-          <Panel>
-            <div className={styles["preview"]}>It is preview</div>
-          </Panel>
-        </PanelGroup>
-      </main>
+    <div className={clsx(styles.page, isCollapsed && styles.collapsed)}>
+      <div className={styles.toolbar}>Toolbar</div>
+      <div className={clsx(styles.sidebar)}>Sidebar</div>
+      <ButtonComponent
+        className={styles.toggle}
+        variant="text"
+        color="inherit"
+        size="small"
+        onClick={() => toggleSidebar()}
+      >
+        {isCollapsed ? "<" : ">"}
+      </ButtonComponent>
+      <PanelGroup className={styles.panels} direction="vertical">
+        <Panel minSize={20}>
+          <div className={styles.pipeline}>Pipeline</div>
+        </Panel>
+        <PanelResizeHandle />
+        <Panel minSize={20}>
+          <div className={styles.preview}>Preview</div>
+        </Panel>
+      </PanelGroup>
     </div>
   );
 }
