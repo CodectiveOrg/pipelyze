029dc3341982860bf75d2a68d2ccef77e500105e
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2025-08-10 11:48:53 +0330

    [ADD] rows of data to data grid essentials

--- src/components/data-grid-essentials/components/data-grid-essentials-row/data-grid-essentials-row.component.tsx
+++ src/components/data-grid-essentials/components/data-grid-essentials-row/data-grid-essentials-row.component.tsx
@@ -1,7 +1,21 @@
 import { ReactElement } from "react";
 
+import { DataGridEssentialsRowType } from "@/components/data-grid-essentials/types/data-grid-essentials-row.type";
+
 import styles from "./data-grid-essentials-row.module.css";
 
-export default function DataGridEssentialsRowComponent(): ReactElement {
-  return <div className={styles["data-grid-essentials-row"]}></div>;
+type Props = {
+  item: DataGridEssentialsRowType;
+};
+
+export default function DataGridEssentialsRowComponent({
+  item,
+}: Props): ReactElement {
+  console.log(item);
+
+  return (
+    <div className={styles["data-grid-essentials-row"]}>
+      Look Logs in the Console
+    </div>
+  );
 }
