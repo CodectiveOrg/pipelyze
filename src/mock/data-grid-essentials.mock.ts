d94ecbaa388c215d63c95e55d39501ff87c1e4ac
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2025-08-10 11:43:42 +0330

    [ADD] mock data for data grid essentials

---
+++ src/mock/data-grid-essentials.mock.ts
@@ -0,0 +1,13 @@
+import { DataGridEssentialsRowType } from "@/components/data-grid-essentials/types/data-grid-essentials-row.type";
+
+export const DATA_GRID_ESSENTIALS_MOCK_ROWS: DataGridEssentialsRowType[] = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: "Ali", age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
