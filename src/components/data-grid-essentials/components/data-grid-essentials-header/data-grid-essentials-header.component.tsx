285b373d1fb2cb9534ca4e54c8bdc89136cb3ccc
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2025-08-10 10:40:22 +0330

    [ADD] data grid components (header & row)

---
+++ src/components/data-grid-essentials/components/data-grid-essentials-header/data-grid-essentials-header.component.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+import styles from "./data-grid-essentials-header.module.css";
+
+export default function DataGridEssentialsHeaderComponent(): ReactElement {
+  return <div className={styles["data-grid-essentials-header"]}></div>;
+}
