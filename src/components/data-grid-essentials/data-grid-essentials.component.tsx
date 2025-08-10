78851cbfba5344637ea5b8427fb19b17720a7e55
SinaMAmini <smobasheramini@gmail.com>
2025-08-10 10:13:03 +0330

    [INIT]

---
+++ src/components/data-grid-essentials/data-grid-essentials.component.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+import styles from "./data-grid-essentials.module.css";
+
+export default function DataGridEssentialsComponent(): ReactElement {
+  return <div className={styles["data-grid-essentials"]}></div>;
+}
