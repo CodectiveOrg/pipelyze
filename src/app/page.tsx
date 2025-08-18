0acedf1bc2ce9850c5791873acb73db75b2f8167
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2025-08-18 20:33:10 +0330

    move handle fetch on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
+"use client";
+
 import { ReactNode } from "react";
 
-import HeroComponent from "@/components/hero/hero.component";
+import FileExplorerComponent from "@/components/file-explorer/file-explorer.component";
+import { type FileExplorerItemType } from "@/components/file-explorer/type/file-explorer-item.type";
 
 import styles from "./page.module.css";
 
 export default function Page(): ReactNode {
+  const handleFetch = async (
+    page: number,
+    pageSize: number,
+  ): Promise<FileExplorerItemType[]> => {
+    const res = await fetch(
+      `http://localhost:3000/api/dataset?page=${page}&pageSize=${pageSize}`,
+    );
+    const data = await res.json();
+    const dataset: FileExplorerItemType[] = await data.result.datasets;
+
+    return dataset;
+  };
+
   return (
     <div className={styles.page}>
-      <HeroComponent />
+      <FileExplorerComponent items={[]} onFetch={handleFetch} />
     </div>
   );
 }
--- src/components/file-explorer/file-explorer.component.tsx
+++ src/components/file-explorer/file-explorer.component.tsx
@@ -1,26 +1,30 @@
+"use client";
+
 import type { ReactNode } from "react";
 
 import FileExplorerHeaderComponent from "@/components/file-explorer/components/file-explorer-header/file-explorer-header.component";
 import FileExplorerRowComponent from "@/components/file-explorer/components/file-explorer-row/file-explorer-row.component";
-import { FileExplorerItemType } from "@/components/file-explorer/type/file-explorer-item.type";
+import { type FileExplorerItemType } from "@/components/file-explorer/type/file-explorer-item.type";
+
+import PaginationComponent from "../pagination/pagination.component";
 
 import styles from "./file-explorer.module.css";
 
-export default async function FileExplorerComponent(): Promise<ReactNode> {
-  const res = await fetch(
-    "http://localhost:3000/api/dataset?page=1&pageSize=2",
-  );
-  const data = await res.json();
-  const dataset: FileExplorerItemType[] = await data.result.datasets;
+type Props = {
+  items: FileExplorerItemType[];
+  onFetch: (page: number, pageSize: number) => Promise<FileExplorerItemType[]>;
+};
 
+export default function FileExplorerComponent({ items }: Props): ReactNode {
   return (
     <div className={styles["file-explorer"]}>
       <div className={styles.table}>
         <FileExplorerHeaderComponent />
-        {dataset.map((item) => (
+        {items.map((item) => (
           <FileExplorerRowComponent key={item.id} item={item} />
         ))}
       </div>
+      <PaginationComponent />
     </div>
   );
 }
